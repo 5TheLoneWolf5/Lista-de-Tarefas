4669043f8ac40026c816da2fdc1453444f80c879
Felipe Alves <89516641+5TheLoneWolf5@users.noreply.github.com>
2023-02-08 17:23:26 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,75 @@
+const inserir = () => {
+
+  const novaTarefa = document.getElementById("novaTarefa");
+  const listaTarefas = document.getElementById("listaTarefas");
+
+  if (novaTarefa.value != "") {
+
+     listaTarefas.innerHTML += novaTarefa.value + "<br>";
+     novaTarefa.value = "";
+     novaTarefa.focus();
+
+  } else {
+
+    return;
+
+  }
+};
+
+const pesquisar = function() {
+
+  const listaTarefas = document.getElementById("listaTarefas");
+  const html = listaTarefas.innerHTML;
+  const tarefas = html.split("<br>"); // Array de strings.
+  // console.log(tarefas);
+
+  const textoTarefa = document.getElementById("textoTarefa");
+  const texto = textoTarefa.value;
+  let resultadoPesquisa = tarefas.filter(item => item.indexOf(texto) >= 0);
+  alert(resultadoPesquisa);
+
+};
+
+const alterar = () => {
+  
+  const textoTarefa = document.getElementById("textoTarefa");
+  const novaTarefa = document.getElementById("novaTarefa");
+  const listaTarefas = document.getElementById("listaTarefas");
+
+  let dividir = listaTarefas.innerHTML.split("<br>");
+  let achar = listaTarefas.
+    
+  novaTarefa.value = listaTarefas.value;
+  textoTarefa.value = "";
+  
+}
+
+const excluir = () => {
+
+  let textoTarefa = document.getElementById("textoTarefa");
+  let listaTarefas = document.getElementById("listaTarefas");
+
+  let tarefaSeparada = listaTarefas.innerHTML.split("<br>");
+
+  if (tarefaSeparada.find(item => item == textoTarefa.value)) {
+
+    listaTarefas.innerHTML = "";
+
+    let filtro = tarefaSeparada.filter(element => element !== textoTarefa.value);
+    console.log(filtro);
+
+    for (i = 0; i < filtro.length; i++) {
+
+      listaTarefas.innerHTML += filtro[i] + "<br>";
+
+    }
+
+    textoTarefa.value = "";
+
+  } else {
+
+    return;
+
+  }
+
+};
